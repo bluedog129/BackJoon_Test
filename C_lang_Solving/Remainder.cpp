d315875f3d97af35d2565cea5632e4dfca707122
bluedog129 <bluedog129@gmail.com>
2021-06-02 22:40:04 +0900

    Remainder 수정1

--- C_lang_Solving/Remainder.cpp
+++ C_lang_Solving/Remainder.cpp
@@ -3,18 +3,26 @@
 int main() {
 	int num[10] = { 0 };
 	int Cnt = 0;
-
-	for (int i = 0; i < 3; i++) {
-		for (int i = 0; i < sizeof(num) / sizeof(int); i++) {
-			scanf_s("%d", &num[i]);
-			if (num[i] == 1) {
-
-			}
+	int rmd = 0;
+	int rmdArr[10] = { 0 };
+	for (int z = 0; z < 3; z++) {
+		// 전체 3번 반복
+		for (int n = 0; n < 10; n++) {
+			//10개의 정수를 입력받음
+			scanf_s("%d", &num[n]);
 		}
-		
+		for (int i = 0; i < sizeof(num) / sizeof(int); i++) {
+			for (int j = 0; j < sizeof(num) / sizeof(int); j++) {
+				// rmd = num[]에 42를 나눈 나머지값
+				rmdArr[j] = num[j] % 42;
+				// 
+				rmd = rmdArr[j];
 
-		printf("%d", Cnt);
+				if (rmdArr[i] == rmd) {
+
+				}
+			}
+			printf("%d", Cnt);
+		}
 	}
-
-	
 }

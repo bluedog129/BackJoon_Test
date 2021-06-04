69bee79b2d5794f86bdcd567d5cf7d9aa8257e74
bluedog129 <bluedog129@gmail.com>
2021-06-04 23:31:00 +0900

    arrEverage 수정

--- C_lang_Solving/arrEverage.cpp
+++ C_lang_Solving/arrEverage.cpp
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main() {
+	int M = 0;
+	int N = 0;
+	int num[] = { 0 };
+	int sum = 0;
+	int everage = 0;
+
+	scanf_s("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &num[i]);
+		if (num[i] > M) {
+			M = num[i];
+		}
+		sum += num[i];
+	}
+	
+	everage = sum / M * 100 / N;
+
+	printf("%d", everage);
 
 }

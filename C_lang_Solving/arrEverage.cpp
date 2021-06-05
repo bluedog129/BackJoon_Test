7ee3eb302f1a5c11324495c21bdb8ee52cf49ed5
bluedog129 <bluedog129@gmail.com>
2021-06-05 13:05:04 +0900

    arrEverage 수정2

--- C_lang_Solving/arrEverage.cpp
+++ C_lang_Solving/arrEverage.cpp
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
+//Á¶°Ç
+//N <= 1000  ,  grade[] <= 100 && grade[] >= 0
+
 int main() {
 	int M = 0;
 	int N = 0;
-	int num[] = { 0 };
-	int sum = 0;
-	int everage = 0;
+	int grade[1000] = { 0 };
+	float sum = 0;
+	float everage = 0;
 
 	scanf_s("%d", &N);
 
 	for (int i = 0; i < N; i++) {
-		scanf_s("%d", &num[i]);
-		if (num[i] > M) {
-			M = num[i];
+		scanf_s("%d", &grade[i]);
+		if (grade[i] > M) {
+			M = grade[i];
 		}
-		sum += num[i];
 	}
 	
-	everage = sum / M * 100 / N;
+	for (int i = 0; i < N; i++) {
+		grade[i] = (grade[i] / M) * 100;
+		sum += grade[i];
+	}
 
-	printf("%d", everage);
+	everage = (float)sum / N;
 
+	printf("%.10f", everage);
+
+	return 0;
 }

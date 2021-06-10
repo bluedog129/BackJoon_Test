ab8841dd858515df652d2484a0f33bbd991ba083
bluedog129 <bluedog129@gmail.com>
2021-06-10 23:01:14 +0900

    OXquiz 수정, OverEv 추가

--- C_lang_Solving/OXquiz.cpp
+++ C_lang_Solving/OXquiz.cpp
@@ -1,25 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	int input = 0;
-	char OX[100] = { 0 };
-	int count = 0;
-	int sum = 0;
+	char OX[81] = { 0 };
+	int size = strlen(OX);
+
 
 	scanf_s("%d", &input);
 
 	for (int i = 0; i < input; i++) {
-		scanf_s("%s", OX,sizeof(OX));
-		
-		if (OX[i] == 'O') {
-			count++;
-		}
-		else if (OX[i] == 'X') {
-			count = 0;
-		}
+		scanf_s("%s", OX, sizeof(OX));
 
-		sum += count;
+		int count = 0;
+		int sum = 0;
 
+		for (int j = 0; j < size; j++) {
+			if (OX[i] == 'O') {
+				count++;
+			}
+			else if (OX[i] == 'X') {
+				count = 0;
+			}
+			sum += count;
+		}
 		printf("%d\n", sum);
 	}
+
+	return 0;
 }
---
+++ C_lang_Solving/OverEv.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int C = 0;
+	int N[1001] = { 0 };
+	int score[] = { 0 };
+
+	scanf_s("%d", &C);
+
+	for (int i = 0; i < C; i++) {
+		scanf_s("%d", &N);
+		printf("%d ", N);
+
+		
+	}
+
+
+}

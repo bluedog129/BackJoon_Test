cb2404bc7cb2954e4b8b1bf9ab3e16e5f0302565
bluedog129 <bluedog129@gmail.com>
2021-06-17 18:29:07 +0900

    ThreeInt 정답

--- C_lang_Solving/ThreeInt.cpp
+++ C_lang_Solving/ThreeInt.cpp
@@ -3,7 +3,6 @@
 int main() {
 	int A, B, C;
 	int arr[3] = { 0 };
-	int Max = 0;
 	int temp = 0;
 	scanf_s("%d %d %d", &A, &B, &C);
 
@@ -13,14 +12,14 @@ int main() {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = i; j < 3; j++) {
-			if (arr[j] > Max) {
-				Max = arr[j];
+			// j번째가 i 번째보다 큰 조건
+			if (arr[j] > arr[i]) {
+				// 수가 더 큰 j인덱스 값과 원래 i인덱스 위치를 서로 바꿔준다.
 				temp = arr[i];
-				arr[i] = Max;
+				arr[i] = arr[j];
 				arr[j] = temp;
 			}
 		}
 	}
-
 	printf("%d", arr[1]);
 }

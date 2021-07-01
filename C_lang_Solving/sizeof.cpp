12aa5e645399639264666358433427bea32b6771
bluedog129 <bluedog129@gmail.com>
2021-07-02 00:18:56 +0900

    strlen

--- C_lang_Solving/sizeof.cpp
+++ C_lang_Solving/sizeof.cpp
@@ -16,7 +16,7 @@ int main() {
 	unsigned int int_size2 = sizeof(int);
 	unsigned int int_size3 = sizeof(a);
 
-	size_t int_size4 = sizeof(a);
+	size_t int_size4 = sizeof(a); 
 	size_t float_size = sizeof(float);
 
 	cout << int_size1 << '\n';

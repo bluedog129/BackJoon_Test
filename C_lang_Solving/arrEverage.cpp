4550e368896dd6a18a876285a34838f09b55b5ee
CHOI HYOJONG <50707297+bluedog129@users.noreply.github.com>
2021-07-11 23:10:13 +0900

    arrEverage 정답

--- C_lang_Solving/arrEverage.cpp
+++ C_lang_Solving/arrEverage.cpp
@@ -1,34 +1,27 @@
 #include <stdio.h>
 
-//Á¶°Ç
-//N <= 1000  ,  grade[] <= 100 && grade[] >= 0
-
-
 int main() {
 	int M = 0;
 	int N = 0;
-	double grade[100] = { 0 };
+	int score[1000];
 	double sum = 0;
-	double everage = 0;
+	double newScore[1000];
 
-	scanf_s("%d", &N);
+	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
-		scanf_s("%f", &grade[i]);
-		if (grade[i] > M) {
-			M = grade[i];
+		scanf("%d", &score[i]);
+		if (score[i] > M) {
+			M = score[i];
 		}
 	}
 	
 	for (int i = 0; i < N; i++) {
-		grade[i] = (grade[i] / M) * 100;
-		printf("%.2lf\n", grade[i]);
-		sum += grade[i];
+		newScore[i] = (double)score[i] / (double)M * 100;
+		sum += newScore[i];
 	}
 
-	everage = (float)sum / N;
-
-	printf("%.2lf\n", everage);
+	printf("%.4lf\n", (double)(sum / N));
 
 	return 0;
 }

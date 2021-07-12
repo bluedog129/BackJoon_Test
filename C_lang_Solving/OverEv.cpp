549a2fb9ed70ec67de35b1316c85c70c89989173
CHOI HYOJONG <50707297+bluedog129@users.noreply.github.com>
2021-07-12 17:51:35 +0900

    overEv 정답

--- C_lang_Solving/OverEv.cpp
+++ C_lang_Solving/OverEv.cpp
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
 int main() {
-	int C = 0;
-	int N[1001] = { 0 };
-	int score[] = { 0 };
-
-	scanf_s("%d", &C);
-
+	int C = 0; // 테스트 케이스 갯수
+	int N = 0; // 학생의 수
+	
+	scanf("%d", &C);
+	
 	for (int i = 0; i < C; i++) {
-		scanf_s("%d", &N);
-		printf("%d ", N);
+		scanf("%d", &N);
 
-		
+		int sum = 0;
+		int score[1000] = { 0 }; // 점수
+		double everage = 0.00;
+
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &score[j]);
+			sum += score[j];
+		}
+		// 1. 평균을 때린다.
+		everage = (double)sum / N;
+
+		// 2. 평균보다 점수가 높은 애들 퍼센트를 낸다.
+		int cnt = 0; // 평균보다 점수 높은애들 수 세는 변수
+		for (int j = 0; j < N; j++) {
+			if (everage < score[j]) {
+				cnt++;
+			}
+		}
+		printf("%.3f%%\n", (double)cnt * 100 / N);
 	}
-
-
+	return 0;
 }

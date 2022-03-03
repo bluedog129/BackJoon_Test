926a5d3eb0b5a0bdca0a442b38bb958208450f8f
Choi Hyojong <bluedog129@gmail.com>
2022-03-03 23:57:06 +0900

    matrix

---
+++ C_lang_Solving/matrix.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define MONTHS 12
+#define YEARS 3
+
+int main() {
+	double temp_data[YEARS][MONTHS] = {
+		{},
+		{},
+		{}
+	};
+
+	/* Step 2 */
+	printf("[Temperature Data]\n");
+
+
+
+	/* Step 3 */
+	printf("");
+
+
+}
